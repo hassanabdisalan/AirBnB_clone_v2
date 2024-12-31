3d300a35e0231b3a486c924633ead03d52240da2
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-12-31 17:00:13 +0300

    Debug 1-pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,38 +1,21 @@
-#!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#!/usr/bin/python3
+"""
+This module packs web_static to deploy
+"""
 
-apt-get update
-apt-get install -y nginx
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static
+    """
+    from fabric.api import local
+    from datetime import datetime
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+    local("mkdir -p versions")
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
-    }
-
-    location /redirect_me {
-	return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+    date = datetime.now().strftime('%Y%m%d%H%M%S')
+    path = 'versions/web_static_{}.tgz'.format(date)
+    ret = local('tar -cvzf {} web_static'.format(path))
+    if ret.succeeded:
+        return path
+    else:
+        return None
