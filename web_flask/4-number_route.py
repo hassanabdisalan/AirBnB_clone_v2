d3726aa6d240f4320951242480424fd954237c80
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-02-08 16:51:22 +0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,53 +1,58 @@
 #!/usr/bin/python3
-"""
-Build a Flask application
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
 """
 from flask import Flask
+from flask import abort
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    """
-    return a simple hello message
-    """
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """
-    /hbnb web page
-    """
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_route(text):
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    Custom C urls
-    """
-    return "C is {}".format(text.replace('_', ' '))
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python')
-@app.route('/python/<text>')
-def py_route(text="is cool"):
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
     """
-    Custom python urls
-    """
-    return "Python is {}".format(text.replace('_', ' '))
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>')
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """
-    Custom int number
-    """
+    """Displays 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)\
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
